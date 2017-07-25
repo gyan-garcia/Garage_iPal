389960596341f6873f5761c68a5302ffed1b9733
root <root@yumi.redmond.corp.microsoft.com>
2017-07-24 17:21:07 -0700

    added the structure for the camera callback

--- main.java
+++ main.java
@@ -6,6 +6,7 @@ import MotionPlanner;
 import EmotionExpression;
 public class MainActivity extends Activity {
     private RobotMotion mRobotMotion; 
+    private EmotionExpression expresser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +21,29 @@ public class MainActivity extends Activity {
                 
             }
         });
-       EmotionExpression expresser = new EmotionExpression();
-       expresser.showEmotion("Happiness");
-       mRobotMotion.doAction(RobotMotion.Action.CHEER,0,1000);
+       this.expresser = new EmotionExpression();
+       //Create camera event and listener
+       //in the callback for the camera, call the server. that should run instead of the while loop
+       while (true) {
+           cameraCallback();
+       }
+ 
     }
-     
+    protected void cameraCallback() {
+        //Get camera image from arguments
+        boolean isInBox = isPersonInBox();    
+        if (isInBox) {
+            //Given bounded box image, call the cortana emotion API on it
+            string emotion; //Call microsoft cognitive services API
+            emotion = "Happiness";
+            this.expresser.showEmotion(emotion);
+        }
+    }
     
+    protected boolean isPersonInBox(){
+        return true;
+    }
+
+     
 }
+
