d14ad7cce26c3cf45544ef50290cfb115bd0d46e
Alex LaGrassa <lagrassa@mit.edu>
2017-07-25 20:57:38 -0400

    added hook

--- playground/emo3/src/com/example/emo3/ResponseFromServer.java
+++ playground/emo3/src/com/example/emo3/ResponseFromServer.java
@@ -11,55 +11,38 @@ import android.util.Base64;
 import java.net.*;
 
 public class ResponseFromServer{
-	
-    public InBoxResponse callAPI(byte[] b) throws IOException, JSONException {
-         
+
+	public InBoxResponse callAPI(byte[] b) throws IOException, JSONException {
+
 		//ByteArrayOutputStream baos = new ByteArrayOutputStream();  
 		//bm.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object   
-		
+
 		String encodedImage = Base64.encodeToString(b , Base64.DEFAULT);
-		
-        HttpURLConnection urlConnection = null;
-        URL url = new URL("IP address");
-        urlConnection = (HttpURLConnection) url.openConnection();
-        urlConnection.setRequestMethod("GET");
-        urlConnection.setReadTimeout(10000 /* milliseconds */ );
-        urlConnection.setConnectTimeout(15000 /* milliseconds */ );
-        urlConnection.setDoOutput(true);
-        urlConnection.connect();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-        StringBuilder sb = new StringBuilder();
 
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line + "\n");
-        }
-        br.close();
-        String jsonString = sb.toString();
-        System.out.println("JSON: " + jsonString);
+		//FIXME send encodedImage, get JSON
 
-        JSONObject obj = new JSONObject(jsonString);
-        
-        boolean inBox = true;
-        String inBoxString = obj.getString("inBox");
-        if (inBoxString.equals("false")) {
-        	inBox = false;
-        } 
-        Double dist = new Double(obj.getString("dist"));
-        String emotion = obj.getString("emotion");
-        return new InBoxResponse(inBox, dist, emotion);
-    }
+		JSONObject obj = new JSONObject(jsonString);
 
-    public class InBoxResponse{
-        public boolean inBox;
-        public double dist;
-        public String emotion;
-        public InBoxResponse(boolean inBox, double inputDist, String inputEmotion) {
-            this.inBox = inBox;
-            this.dist = inputDist; 
-            this.emotion = inputEmotion;
-        }
-    }
+		boolean inBox = true;
+		String inBoxString = obj.getString("inBox");
+		if (inBoxString.equals("false")) {
+			inBox = false;
+		} 
+		Double dist = new Double(obj.getString("dist"));
+		String emotion = obj.getString("emotion");
+		return new InBoxResponse(inBox, dist, emotion);
+	}
+
+	public class InBoxResponse{
+		public boolean inBox;
+		public double dist;
+		public String emotion;
+		public InBoxResponse(boolean inBox, double inputDist, String inputEmotion) {
+			this.inBox = inBox;
+			this.dist = inputDist; 
+			this.emotion = inputEmotion;
+		}
+	}
 }
 
