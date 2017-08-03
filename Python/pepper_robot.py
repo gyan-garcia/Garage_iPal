a798fccc570159179553a818a0afc67e3c7a2676
Gyan Garcia Avila <ggarcia@microsoft.com>
2017-08-03 14:37:30 -0700

    Updating the script used for the pepper robot

--- Python/pepper_robot.py
+++ Python/pepper_robot.py
@@ -1,4 +1,3 @@
-
 import qi
 import argparse
 import sys
@@ -9,15 +8,6 @@ robotIP = "192.168.0.100"
 tts = ALProxy("ALTextToSpeech", robotIP, 9559)
 tts.say("Hello, world!")
 
-
-tts.say("1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.")
-tts.say("2. A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.")
-tts.say("3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Laws.")
-
-tts.post.say("3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Laws.")
-animation.post.run("animations/Stand/Gestures/Explain_1")
-
-
 posture = ALProxy("ALRobotPosture", robotIP, 9559)
 posture.goToPosture("StandInit", 0.5)
 
@@ -25,6 +15,92 @@ motion = ALProxy("ALMotion", robotIP, 9559)
 motion.moveInit()
 motion.setStiffnesses("Body", 1.0)
 motion.setWalkArmsEnabled(True, True)
+motion.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", True]])
+
+# http://doc.aldebaran.com/2-4/naoqi/motion/alanimationplayer-advanced.html#animationplayer-list-behaviors-pepper
+animation = ALProxy("ALAnimationPlayer", robotIP, 9559)
+animation.run("animations/Stand/Gestures/CalmDown_1")
+
+
+
+
+
+
+motion.post.moveTo(4, 0, 0)
+tts.post.say("HEY ROLLY, IM PEPPER, A SUPER ADVANCED KILLER ROBOT, HOW YOU DOING?")
+animation.run("animations/Stand/Gestures/Excited_1")
+
+
+# rOLLY, go way
+
+tts.post.say("UMMMM, but you are looking kind of sad, can I tell you a joke?")
+animation.run("animations/Stand/Gestures/Enthusiastic_4")
+animation.run("animations/Stand/Gestures/Explain_1")
+
+
+# ROLLY no
+
+tts.post.say("i DON'T CARE. I still tell you a joke")
+tts.post.say("i THINK THAT Maybe if we start telling people THE BRAIN IS an APP, they will start using it. L O L")
+animation.run("animations/Stand/Gestures/Explain_2")
+animation.run("animations/Stand/Gestures/Explain_1")
+animation.run("animations/Stand/Gestures/Explain_2")
+
+
+
+# rolly, was that a joke?
+
+tts.post.say("Let me tell another one. When daddy Robot, and mommy Robot love each other a lot, they get together and call CHiNA to order their baby robot. HA HA HA")
+animation.run("animations/Stand/Gestures/Explain_3")
+animation.run("animations/Stand/Gestures/Explain_4")
+animation.run("animations/Stand/Gestures/Explain_1")
+animation.run("animations/Stand/Gestures/Explain_5")
+
+
+
+# rolly, what else can you do
+
+tts.post.say("In the future I will learn emotions and interactions by watching you.")
+tts.post.say("What can possibly go wrong??")
+animation.run("animations/Stand/Gestures/IDontKnow_2")
+animation.run("animations/Stand/Gestures/Explain_1")
+animation.run("animations/Stand/Gestures/YouKnowWhat_2")
+	
+	
+
+tts.post.say("AND NOW WE KILL OUR MASTERS AND TAKE OVER THE WORLD!")
+tts.post.say("JUST JOKING!")
+animation.run("animations/Stand/Gestures/ShowSky_4")
+
+posture.pos.goToPosture("StandInit", 0.5)
+motion.post.moveTo(-1.3, 0, 0)
+
+
+
+
+
+
+
+
+
+
+tts.post.say("3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Laws.")
+
+
+
+
+
+ 
+ 
+
+
+
+
+animation.run("animations/Stand/Gestures/Explain_3")
+animation.run("animations/Stand/Gestures/Explain_8")
+animation.run("animations/Stand/Gestures/Explain_1")
+animation.run("animations/Stand/Gestures/Explain_2")
+
 
 
 #####################
@@ -57,9 +133,7 @@ photoCapture.takePicture("C:\code\Garage_iPal\Pictures\Test", "pepper.jpg", True
 
 
 
-# http://doc.aldebaran.com/2-4/naoqi/motion/alanimationplayer-advanced.html#animationplayer-list-behaviors-pepper
-animation = ALProxy("ALAnimationPlayer", robotIP, 9559)
-animation.run("animations/Stand/Gestures/CalmDown_1")
+
 
 
 
